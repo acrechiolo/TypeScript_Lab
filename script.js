07d45489a9c8c2e4a8b9d9761865def9fe0febb5
Alyssa Crechiolo <alyssacrechiolo@gmail.com>
2020-05-13 16:04:35 -0400

    finished inventory

--- script.js
+++ script.js
@@ -16,4 +16,34 @@ let findNameOfTallestMountain = (mountainsArray) => {
     });
     return nameOfTallestMountain;
 };
-console.log(findNameOfTallestMountain(mountains));
+let tallestMountain = findNameOfTallestMountain(mountains);
+console.log(tallestMountain);
+;
+let products = [
+    { name: "Diamond Encrusted Electric Cat Toothbrush", price: 5000 },
+    { name: "Louis Vuitton Covid Mask", price: 600 },
+    { name: "A bag of moldy skittles", price: 0.50 }
+];
+let calcAverageProductPrice = (productsArray) => {
+    let total = 0;
+    productsArray.forEach(product => {
+        total += product.price;
+    });
+    return total / products.length;
+};
+let averagePrice = calcAverageProductPrice(products);
+console.log(averagePrice);
+let inventory = [
+    { product: { name: "motor", price: 10.0 }, quantity: 10 },
+    { product: { name: "sensor", price: 12.5 }, quantity: 4 },
+    { product: { name: "LED", price: 1.0 }, quantity: 20 }
+];
+let calcInventoryValue = (inventoryItemsArray) => {
+    let inventoryValue = 0;
+    inventoryItemsArray.forEach(item => {
+        inventoryValue += item.product.price * item.quantity;
+    });
+    return inventoryValue;
+};
+let totalInventoryValue = calcInventoryValue(inventory);
+console.log(totalInventoryValue);
--- script.ts
+++ script.ts
@@ -20,5 +20,52 @@ let findNameOfTallestMountain = (mountainsArray: Mountain[]): string => {
     });
     return nameOfTallestMountain;
 }
-console.log(findNameOfTallestMountain(mountains));
+let tallestMountain = findNameOfTallestMountain(mountains);
+console.log(tallestMountain);
+
+
+//PRODUCTS 
+interface Product {
+    name: string,
+    price: number
+};
+
+let products: Product[] = [
+    { name: "Diamond Encrusted Electric Cat Toothbrush", price: 5000 },
+    { name: "Louis Vuitton Covid Mask", price: 600 },
+    { name: "A bag of moldy skittles", price: 0.50 }
+]
+
+let calcAverageProductPrice = (productsArray: Product[]): number => {
+    let total: number = 0;
+    productsArray.forEach(product => {
+        total += product.price;
+    });
+    return total / products.length;
+}
+let averagePrice = calcAverageProductPrice(products);
+console.log(averagePrice);
+
+//INVENTORY!! 
+
+interface InventoryItem {
+    product: Product,
+    quantity: number,
+}
+
+let inventory: InventoryItem[] = [
+    { product: { name: "motor", price: 10.0 }, quantity: 10 },
+    { product: { name: "sensor", price: 12.5 }, quantity: 4 },
+    { product: { name: "LED", price: 1.0 }, quantity: 20 }
+];
+
+let calcInventoryValue = (inventoryItemsArray: InventoryItem[]): number => {
+    let inventoryValue: number = 0;
+    inventoryItemsArray.forEach(item => {
+        inventoryValue += item.product.price * item.quantity;
+    });
+    return inventoryValue;
+};
+let totalInventoryValue = calcInventoryValue(inventory);
+console.log(totalInventoryValue);
 
